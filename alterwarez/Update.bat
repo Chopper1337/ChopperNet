c06c4b393da78fce4d415bedd90c2a06ba4d36b2
Chopper1337 <43015696+Chopper1337@users.noreply.github.com>
2020-11-11 22:55:20 +0000

    Automatically updated

--- alterwarez/Update.bat
+++ alterwarez/Update.bat
@@ -1,5 +1,9 @@
+@echo off
+title AlterWarezUpdateScript
+CMDOW AlterWarezUpdateScript /MIN
 timeout 10
 start AlterWarezUpdater.exe
+CMDOW AlterWarezUpdater /MIN
 timeout 20
 git commit -a -m "Automatically updated"
 git push -u origin master
